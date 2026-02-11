cd08e75e027296b35a604cc00fa72ff7a4243fd8
Xuwznln <18435084+Xuwznln@users.noreply.github.com>
2026-02-11 16:34:56 +0800

    holder fix

--- pylabrobot/resources/tube_rack.py
+++ pylabrobot/resources/tube_rack.py
@@ -75,4 +75,4 @@ class TubeRack(ItemizedResource[ResourceHolder]):
       The tube at the given position, or None if there is no tube at that position.
     """
     holder = self.get_item(key)
-    return cast(Optional[Tube], holder.resource) if holder.resource is not None else None
+    return holder.resource if holder.resource is not None else holder  # need fix
